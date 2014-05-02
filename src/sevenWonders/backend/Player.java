69bd1ba14d4089f7446cf792c28734623b2bb68b
Andreas Jönsson <superraketen@hotmail.com>
2014-05-02 17:53:59 +0200

    Changed Player regarding victories/losses

--- src/sevenWonders/backend/Player.java
+++ src/sevenWonders/backend/Player.java
@@ -6,56 +6,58 @@ import java.util.List;
 public class Player {
     public final Wonder wonder;
     private List<Card> buildings = new ArrayList<>();
-    private int money; // TODO
-    private int militaryWins = 0; // TODO: left/right?
-    private int militaryLosses = 0;
+    private int money;
     public final String name;
     
+    // -1 means a loss 0 means draw, otherwise victory
+    private int leftMilitaryWins[] = { 0, 0, 0 };
+    private int rightMilitaryWins[] = { 0, 0, 0 };
+
     /**
      * Creates a new player with passed attributes
-     * @param name The player's name
-     * @param wonder The player's wonder
+     * 
+     * @param name
+     *            The player's name
+     * @param wonder
+     *            The player's wonder
      */
     public Player(String name, Wonder wonder) {
 	this.wonder = wonder;
 	this.name = name;
     }
-    
+
     public int getMoney() {
 	return money;
     }
-    
+
     /**
-     * Does not have a access modifier because it should only 
-     * be accessible from this package.
+     * Does not have a access modifier because it should only be accessible from
+     * this package.
      */
     void addMoney(int m) {
 	money += m;
     }
-    
+
     /**
-     * Does not have a access modifier because it should only 
-     * be accessible from this package.
+     * Does not have a access modifier because it should only be accessible from
+     * this package.
      */
     void removeMoney(int m) {
 	if (m > money)
 	    throw new IllegalArgumentException("Not enough funds");
 	money -= m;
     }
-    
-    public int getMilitaryWins() {
-	return militaryWins;
+
+    public int[][] getMilitaryWins() {
+	return new int[][] { leftMilitaryWins, rightMilitaryWins };
     }
-    
-    public int getMilitaryLosses() {
-	return militaryLosses;
-    }
-    
+
     /**
      * Adds the passed card to the player's buildings. Doesn't check resources.
+     * 
      * @param building
-     * Does not have a access modifier because it should only 
-     * be accessible from this package.
+     *            Does not have a access modifier because it should only be
+     *            accessible from this package.
      */
     void buildCard(Card building) {
 	buildings.add(building);
