26b682b8cbc347137949cceebabdcfc9bb820c44
Andreas Jönsson <superraketen@hotmail.com>
2014-05-07 22:08:43 +0200

    Spaces and tabs acted up

--- src/sevenWonders/frontend/PlayerBoard.java
+++ src/sevenWonders/frontend/PlayerBoard.java
@@ -46,18 +46,18 @@ public class PlayerBoard extends AnchorPane implements Initializable {
     private Player player;
     
     public PlayerBoard(Player player) {
-	this.player = player;
-	stageObjects = new ImageView[player.wonder.stages.length];
-	
-	FXMLLoader loader = new FXMLLoader(Program.getURL("PlayerBoard.fxml"));
-	loader.setRoot(this);
-	loader.setController(this);
-	
-	try {
-	    loader.load();
-	} catch (IOException e) {
-	    throw new RuntimeException(e);
-	}
+        this.player = player;
+        stageObjects = new ImageView[player.wonder.stages.length];
+
+        FXMLLoader loader = new FXMLLoader(Program.getURL("PlayerBoard.fxml"));
+        loader.setRoot(this);
+        loader.setController(this);
+
+        try {
+            loader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
     
     /**
@@ -65,104 +65,102 @@ public class PlayerBoard extends AnchorPane implements Initializable {
      */
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
-	// Initialize card placeholders
-    	int offsetX = 15, offsetY = 30, stackSize = 6,
-    	    stackOffsetX = 110, stackOffsetY = -28;
-    	for (int i = 0; i < NUMCARDS; i++) {
-    	    
-    	    ImageView v = new ImageView();
-    	    v.setLayoutX(BaseCard.getLayoutX() + offsetX*(i%stackSize) + stackOffsetX*(i/stackSize));
-    	    v.setLayoutY(BaseCard.getLayoutY() + offsetY*(i%stackSize) + stackOffsetY*(i/stackSize));
-    	    
-    	    v.setPreserveRatio(true);
-    	    v.setFitHeight(BaseCard.getFitHeight());
-    	    v.setFitWidth(BaseCard.getFitWidth());
-    	    
-    	    cardsObjects[i] = v;
-    	    this.getChildren().add(v);
-    	}
-    	
-    	ImageView last = cardsObjects[NUMCARDS - 1];
-    	ImageView secondLast = cardsObjects[NUMCARDS - 2];
-    	last.setLayoutX(secondLast.getLayoutX() + offsetX);
-    	last.setLayoutY(secondLast.getLayoutY() + offsetY);
-    	
-    	this.getChildren().remove(BaseCard);
-	
-	// Initialize stages of the Wonder
-    	Wonder wonder = player.wonder;
-    	int offsetStep = 65 - (int)BaseWonderStage.getFitWidth();
-    	if (wonder.stages.length == 4) {
-    	    offsetStep = 0;
-    	    BaseWonderStage.setLayoutY(BaseWonderStage.getLayoutY() - 20);
-    	}
-    	
-    	for (int i = 0, idx = wonder.stages.length-1; i < wonder.stages.length; i++, idx--) {
-    	    ImageView v = new ImageView();
-    	    v.setLayoutY(BaseWonderStage.getLayoutY() + i*offsetStep + i*BaseWonderStage.getFitWidth());
-    	    
-    	    v.setRotate(BaseWonderStage.getRotate());
-    	    v.setLayoutX(BaseWonderStage.getLayoutX());
-    	    v.setPreserveRatio(true);
-    	    v.setFitHeight(BaseWonderStage.getFitHeight());
-    	    v.setFitWidth(BaseWonderStage.getFitWidth());
-    	    
-    	    stageObjects[idx] = v;
-    	    this.getChildren().add(v);
-    	}
-    	this.getChildren().remove(BaseWonderStage);
-    	this.getChildren().remove(WonderImage);
-    	this.getChildren().add(WonderImage);
-    	
-    	// Initialize Military token placeholders
-    	int offsetMilitary = 9;
-    	for (int i = 0; i < 3; i++) {
-    	    ImageView l = new ImageView();
-    	    ImageView r = new ImageView();
+    // Initialize card placeholders
+        int offsetX = 15, offsetY = 30, stackSize = 6,
+            stackOffsetX = 110, stackOffsetY = -28;
+        for (int i = 0; i < NUMCARDS; i++) {
+            
+            ImageView v = new ImageView();
+            v.setLayoutX(BaseCard.getLayoutX() + offsetX*(i%stackSize) + stackOffsetX*(i/stackSize));
+            v.setLayoutY(BaseCard.getLayoutY() + offsetY*(i%stackSize) + stackOffsetY*(i/stackSize));
+            
+            v.setPreserveRatio(true);
+            v.setFitHeight(BaseCard.getFitHeight());
+            v.setFitWidth(BaseCard.getFitWidth());
+            
+            cardsObjects[i] = v;
+            this.getChildren().add(v);
+        }
 
-    	    l.setLayoutX(BaseLeftMilitary.getLayoutX() + (offsetMilitary + (int)BaseLeftMilitary.getFitWidth())*i);
-    	    r.setLayoutX(BaseRightMilitary.getLayoutX() - (offsetMilitary + (int)BaseRightMilitary.getFitWidth())*i);
+        ImageView last = cardsObjects[NUMCARDS - 1];
+        ImageView secondLast = cardsObjects[NUMCARDS - 2];
+        last.setLayoutX(secondLast.getLayoutX() + offsetX);
+        last.setLayoutY(secondLast.getLayoutY() + offsetY);
 
-    	    l.setFitHeight(BaseLeftMilitary.getFitHeight());
-    	    r.setFitHeight(BaseRightMilitary.getFitHeight());
-    	    l.setFitWidth(BaseLeftMilitary.getFitWidth());
-    	    r.setFitWidth(BaseRightMilitary.getFitWidth());
-    	    l.setLayoutY(BaseLeftMilitary.getLayoutY());
-    	    r.setLayoutY(BaseRightMilitary.getLayoutY());
+        this.getChildren().remove(BaseCard);
 
-    	    leftMilitary[i] = l;
-    	    rightMilitary[i] = r;
-    	    this.getChildren().add(r);
-    	    this.getChildren().add(l);
-    	}
-    	this.getChildren().remove(BaseLeftMilitary);
-    	this.getChildren().remove(BaseRightMilitary);
-    	
-    	// Displays the proper data
-    	displayData();
+        // Initialize stages of the Wonder
+        Wonder wonder = player.wonder;
+        int offsetStep = 65 - (int)BaseWonderStage.getFitWidth();
+        if (wonder.stages.length == 4) {
+            offsetStep = 0;
+            BaseWonderStage.setLayoutY(BaseWonderStage.getLayoutY() - 20);
+        }
+
+        for (int i = 0, idx = wonder.stages.length-1; i < wonder.stages.length; i++, idx--) {
+            ImageView v = new ImageView();
+            v.setLayoutY(BaseWonderStage.getLayoutY() + i*offsetStep + i*BaseWonderStage.getFitWidth());
+            
+            v.setRotate(BaseWonderStage.getRotate());
+            v.setLayoutX(BaseWonderStage.getLayoutX());
+            v.setPreserveRatio(true);
+            v.setFitHeight(BaseWonderStage.getFitHeight());
+            v.setFitWidth(BaseWonderStage.getFitWidth());
+            
+            stageObjects[idx] = v;
+            this.getChildren().add(v);
+        }
+        this.getChildren().remove(BaseWonderStage);
+        this.getChildren().remove(WonderImage);
+        this.getChildren().add(WonderImage);
+
+        // Initialize Military token placeholders
+        int offsetMilitary = 9;
+        for (int i = 0; i < 3; i++) {
+            ImageView l = new ImageView();
+            ImageView r = new ImageView();
+
+            l.setLayoutX(BaseLeftMilitary.getLayoutX() + (offsetMilitary + (int)BaseLeftMilitary.getFitWidth())*i);
+            r.setLayoutX(BaseRightMilitary.getLayoutX() - (offsetMilitary + (int)BaseRightMilitary.getFitWidth())*i);
+
+            l.setFitHeight(BaseLeftMilitary.getFitHeight());
+            r.setFitHeight(BaseRightMilitary.getFitHeight());
+            l.setFitWidth(BaseLeftMilitary.getFitWidth());
+            r.setFitWidth(BaseRightMilitary.getFitWidth());
+            l.setLayoutY(BaseLeftMilitary.getLayoutY());
+            r.setLayoutY(BaseRightMilitary.getLayoutY());
+
+            leftMilitary[i] = l;
+            rightMilitary[i] = r;
+            this.getChildren().add(r);
+            this.getChildren().add(l);
+        }
+        this.getChildren().remove(BaseLeftMilitary);
+        this.getChildren().remove(BaseRightMilitary);
+
+        // Displays the proper data
+        displayData();
     }
     
     private void displayData() {
-	// Addresses money
-	CoinLabel.setText("" + player.getMoney());
-	
-	// Addresses military
-	for (int i = 0; i < 3; i++) {
-	    int[][] wins = player.getMilitaryWins();
-	    if (wins[0][i] != 0) {
-		leftMilitary[i].setImage(Program.getImageFromFilename("Military" + wins[0][i] + ".png"));
-	    }
-	    if (wins[1][i] != 0) {
-		rightMilitary[i].setImage(Program.getImageFromFilename("Military" + wins[1][i] + ".png"));
-	    }
-	}
-	
-	// Addresses the player's buildings/cards
-	List<Card> buildings = player.getBuildings();
-	for (int i = 0; i < buildings.size(); i++) {
-	    cardsObjects[i].setImage(Program.getImageFromFilename(buildings.get(i).description));
-	}
-	
-	
+        // Addresses money
+        CoinLabel.setText("" + player.getMoney());
+
+        // Addresses military
+        for (int i = 0; i < 3; i++) {
+            int[][] wins = player.getMilitaryWins();
+            if (wins[0][i] != 0) {
+        	leftMilitary[i].setImage(Program.getImageFromFilename("Military" + wins[0][i] + ".png"));
+            }
+            if (wins[1][i] != 0) {
+        	rightMilitary[i].setImage(Program.getImageFromFilename("Military" + wins[1][i] + ".png"));
+            }
+        }
+
+        // Addresses the player's buildings/cards
+        List<Card> buildings = player.getBuildings();
+        for (int i = 0; i < buildings.size(); i++) {
+            cardsObjects[i].setImage(Program.getImageFromFilename(buildings.get(i).description));
+        }
     }
 }
