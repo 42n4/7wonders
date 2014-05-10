114c2ec60b20dcceac93273fe52cbea0ba854491
Jenny Norelius <jenny.norelius@gmail.com>
2014-05-10 14:52:09 +0200

    Added versatility to resourceloader

--- src/sevenWonders/Program.java
+++ src/sevenWonders/Program.java
@@ -2,27 +2,44 @@ package sevenWonders;
 
 import java.io.File;
 import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javafx.scene.image.Image;
 
-
 public class Program {
-    private static String dataFolder = "data/";
+    // Prevent instantiation of this class.
+    private Program() {}
+    private final static String dataFolder = "data/";
+
+    /**
+     * Attempts to load the file path by resource first. If this fails then it
+     * falls back to direct file path instead.
+     */
     public static URL getURL(String filepath) {
-	System.out.print(new File(dataFolder+filepath)); 
+	URL resource = Program.class.getResource("/" + filepath);
+	try {
+	    if (resource != null && new File(resource.toURI()).exists()) {
+		return resource;
+	    }
+	} catch (URISyntaxException e) {
+	    // Do nothing.
+	}
+	System.out.print(new File(dataFolder + filepath));
 	try {
 	    return new File(dataFolder + filepath).toURI().toURL();
 	} catch (MalformedURLException e) {
 	    return null;
 	}
     }
-    
-    private static HashMap<String, Image> loadedImages = new HashMap<String, Image>();
+
+    private final static Map<String, Image> loadedImages = new ConcurrentHashMap<String, Image>();
+
     public static Image getImageFromFilename(String filepath) {
 	Image ret = loadedImages.get(filepath);
-	if (ret == null) { 
+	if (ret == null) {
 	    ret = new Image(getURL(filepath).toString());
 	    loadedImages.put(filepath, ret);
 	}
